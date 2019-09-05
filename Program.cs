08ba14a3136705d5af07c6c49dbab13208035300
Marcos Santos <marcosadriano034@gmail.com>
2019-09-05 00:05:11 -0300

    new console dotnet

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_sharp_in_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
