ba1030a69008cdefcbecdc790506f66eae7f1c35
Marcos Santos <marcosadriano034@gmail.com>
2019-09-08 16:37:22 -0300

    types of the variables and conversions

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace c_sharp_in_practice
 {
@@ -6,7 +8,47 @@ namespace c_sharp_in_practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /** TIPOS DE VARIÁVEIS
+            var nome_variavel = valor; variavel dinamica 
+            <tipo> nome_variavel = valor; variavel estatica
+            */
+
+            // Integer
+            var int2 = 6;
+            int int1 = 3;
+            // Double
+            var double2 = 6.1;
+            double double1 = 3.1;
+            // String
+            var str2 = "Santos";
+            string str1 = "Marcos";
+            // Bool
+            var boll2 = false;
+            bool boll1 = true;
+
+            Console.WriteLine(int1);
+            Console.WriteLine(int2);
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.WriteLine(boll1);
+            Console.WriteLine(boll2);
+
+            /* CONVERSÕES */
+            var v1 = 1.0;
+
+            bool boolean1 = bool.Parse("false");
+            bool boolean2 = Convert.ToBoolean(v1);
+
+            Animal a = new Animal();
+            Leao l = new Leao();
+
+            l = (Leao)a;
+
+            a = l;
         }
+
+        public class Animal { }
+
+        public class Leao : Animal { }
     }
 }
