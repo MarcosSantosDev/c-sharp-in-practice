86d7ff9bb1ef0aacd17651351fea04d2782ed27c
Marcos Santos <marcosadriano034@gmail.com>
2019-09-10 22:56:34 -0300

    learning functions

--- Program.cs
+++ Program.cs
@@ -6,52 +6,43 @@ namespace c_sharp_in_practice
 {
     class Program
     {
+        /** # Funçoes
+            sintax: (local) (tipo) (retorno) (nome) (parametro)
+
+
+            * PascalCase, camelCase, under_line
+        */
+
+        private static int Sum(int a, int b)
+        {
+            int result = a + b;
+
+            return result;
+        }
+
+        public static void ShowHelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void MultiplicationTable(int num)
+        {
+            Console.WriteLine("*=============*==============*");
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(num+" X "+i+": "+(num*i));
+            }
+            Console.WriteLine("*=============*==============*");
+        }
+
         static void Main(string[] args)
         {
-            // # ARRAY
-            Console.WriteLine("## - ARRAY -##");
+            Console.WriteLine(Sum(10, 20));
 
-            string[] names1 = new string[3];
-
-            names1[0] = "Marcos";
-            names1[1] = "Wesley";
-            names1[2] = "Diego";
-
-            foreach (var name in names1)
-            {
-                Console.WriteLine(name);
-            }
-
-            // Exists in Array
-            if (Array.Exists(names1, e => e == "Marcos"))
-            {
-                Console.WriteLine("O nome Marcos foi encontrado no Array.");
-            }
-
-            // Get length array
-            int size_array = names1.Length;
-
-            Console.WriteLine("## - COLLECTION -##");
-            
-            // # COLLECTIONS
-            List<String> names2 = new List<String>();
-            names2.Add("Marcos");
-            names2.Add("Wesley");
-            names2.Add("Diego");
-
-            foreach (var name in names2)
-            {
-                Console.WriteLine(name);
-            }
-
-            // Exists collection
-            if (names2.Exists(e => e == "Marcos"))
-            {
-                Console.WriteLine("O nome Marcos foi encontrado na collection list.");
-            }
-
-            // Get length collection
-            int size_collection = names2.Count();
+            /** TABUADA */
+            MultiplicationTable(1);
+            MultiplicationTable(2);
+            MultiplicationTable(7);
         }
     }
 }
